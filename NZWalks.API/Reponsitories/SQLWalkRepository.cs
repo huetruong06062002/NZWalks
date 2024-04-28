45cebe69f54495e5a08e3389de798a353b1ac726
Đệ giáo-làng <truongvshieu01@gmail.com>
2024-04-28 19:31:35 +0700

    Pagination

--- NZWalks.API/Reponsitories/SQLWalkRepository.cs
+++ NZWalks.API/Reponsitories/SQLWalkRepository.cs
@@ -32,7 +32,7 @@ namespace NZWalks.API.Reponsitories
            return exsingtingWalk;
         }
 
-        public async Task<List<Walk>> GetAllAsync(string? filterOn = null, string? filterQuery = null, string? sortBy = null, bool isAscending = true)
+        public async Task<List<Walk>> GetAllAsync(string? filterOn = null, string? filterQuery = null, string? sortBy = null, bool isAscending = true, int pageNumber = 1, int pageSize = 1000)
         {
 
             var walks = dbContext.Walks.Include("Difficulty").Include("Region").AsQueryable();
@@ -60,7 +60,11 @@ namespace NZWalks.API.Reponsitories
                 }
             }
 
-            return await walks.ToListAsync();
+            //Pagination
+            var skipResults = (pageNumber - 1) * pageSize;
+
+
+            return await walks.Skip(skipResults).Take(pageSize).ToListAsync();
             //return await dbContext.Walks.Include("Difficulty").Include("Region").ToListAsync();
         }
 
